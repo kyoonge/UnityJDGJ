b66e437228fbd875c80edfa3102d5441c3bd5c7b
kyoonge <kyoonge98@gmail.com>
2022-02-08 19:12:05 +0900

    씬 분리작업 전 백업

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerCtrl : MonoBehaviour
 {
@@ -14,16 +15,38 @@ public class PlayerCtrl : MonoBehaviour
     // 회전 속도 변수
     public float turnSpeed = 80.0f;
 
+    //초기 생명 값
+    private readonly float initHp = 100.0f;
+    //현재 생명 값
+    public float currHp;
+    //HPbar 연결할 변수
+    private Image hpBar;
+
+    //델리케이트 선언
+    public delegate void PlayerDieHandler();
+    //이벤트 선언
+    public static event PlayerDieHandler OnPlayerDie;
 
     // Start is called before the first frame update
-    void Start()
+    IEnumerator Start()
     {
+        //HPbar 연결
+        hpBar = GameObject.FindGameObjectWithTag("HP_BAR")?.GetComponent<Image>();
+        //Hp 초기화
+        currHp = initHp;
+        DisplayHealth();
+
         // Transform 컴포넌트를 추출해 변수에 대입
         tr = GetComponent<Transform>();  // 함수명 < 형식(추출할클래스) > ( 매개변수 )
         anim = GetComponent<Animation>();
 
         // 애니메이션 실행
         anim.Play("Idle");
+
+        turnSpeed = 0.0f;
+        yield return new WaitForSeconds(0.3f);
+        turnSpeed = 80.0f;
+
     }
 
     // Update is called once per frame
@@ -56,6 +79,47 @@ public class PlayerCtrl : MonoBehaviour
 
     }
 
+    //충돌한 Collider의 IsTrigger 옵션이 체크됐을 때 발생
+    private void OnTriggerEnter(Collider coll)
+    {
+        //충돌한 Collider가 몬스터의 PUNCH이면 Player의 HP차감
+        if(currHp >= 0.0f && coll.CompareTag("PUNCH"))
+        {
+            currHp -= 10.0f;
+            DisplayHealth();
+
+            Debug.Log($"Player hp = {currHp / initHp}");
+
+            //Player의 생명이 0이하이면 사망 처리
+            if(currHp <= 0.0f)
+            {
+                PlayerDie();
+            }
+        }
+        
+    }
+
+    void PlayerDie()
+    {
+        Debug.Log("Player Die!");
+
+        //MONSTER태그를 가진 모든 게임오브젝트를 찾아옴
+        //GameObject[] monsters = GameObject.FindGameObjectsWithTag("MONSTER");
+
+        //모든 몬스터의 OnPlayerDie 함수를 순차적으로 호출
+        //foreach(GameObject monster in monsters)
+        //{
+        //    monster.SendMessage("OnPlayerDie", SendMessageOptions.DontRequireReceiver);
+        //}
+
+        //주인공 사망 이벤트 호출(발생)
+        OnPlayerDie();
+
+        //GameObject 스크립트의 IsGameOver 프로퍼티 값을 변경
+        //GameObject.Find("GameMgr").GetComponent<GameManager>().IsGameOver = true;
+        GameManager.instance.IsGameOver = true;
+    }
+
     void PlayerAnim(float h, float v)
     {
         // 키보드 입력값에 따라 동작할 애니메이션 수행
@@ -80,5 +144,10 @@ public class PlayerCtrl : MonoBehaviour
         {
             anim.CrossFade("Idle", 0.25f);
         }
+    }
+
+    void DisplayHealth()
+    {
+        hpBar.fillAmount = currHp / initHp;
     }
 }
